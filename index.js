ca32083a0c5a57deaeabada524add3d17079e6e5
slabajos <slabajos@idealista.com>
2018-03-06 20:37:52 +0100

    updated models and allowed CORS

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+// allow cross origin requests:
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
